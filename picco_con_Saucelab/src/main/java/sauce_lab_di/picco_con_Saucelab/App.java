6523cea79de436969d2aaa59ed8534f9667e908e
VigneshS45 <147389673+VigneshS45@users.noreply.github.com>
2024-10-17 19:06:22 +0530

    Add files via upload

---
+++ picco_con_Saucelab/src/main/java/sauce_lab_di/picco_con_Saucelab/App.java
@@ -0,0 +1,13 @@
+package sauce_lab_di.picco_con_Saucelab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ picco_con_Saucelab/src/test/java/sauce_lab_di/picco_con_Saucelab/AppTest.java
@@ -0,0 +1,20 @@
+package sauce_lab_di.picco_con_Saucelab;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
